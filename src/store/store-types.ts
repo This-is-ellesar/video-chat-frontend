610b2d3ed382c2b0ced9eb7a4072745a9966d971
This-is-ellesar <54068776+This-is-ellesar@users.noreply.github.com>
2021-05-13 05:32:31 +0300

    Прописал интерфейсы для снекбара

--- src/store/store-types.ts
+++ src/store/store-types.ts
@@ -1,5 +1,24 @@
 export interface IAuthState {
+    user: User | null
     token: string | null,
     isAuthtentificated: boolean | null,
     isFetching: boolean | null,
+}
+
+export interface IBaseState {
+    snackbar: Snackbar
+}
+
+export interface User {
+    _id: string
+    name: string
+    surname: string
+    roles: string[]
+    nickname: string
+}
+
+export interface Snackbar {
+    type: string 
+    message: string
+    show: boolean
 }
