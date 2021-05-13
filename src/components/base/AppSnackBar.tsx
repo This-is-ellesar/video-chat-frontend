860a2037df70ca30fa18a7d293332f658fec4ca0
This-is-ellesar <54068776+This-is-ellesar@users.noreply.github.com>
2021-05-13 05:29:40 +0300

    Привязал снекбар к его стору

--- src/components/base/AppSnackBar.tsx
+++ src/components/base/AppSnackBar.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react'
+import { useStore } from 'react-redux'
 //ui
 import { Snackbar } from '@material-ui/core';
 
 const AppSnackBar: FC = () => {
+    const snackbar = useStore().getState().base
+
+    console.log(snackbar)
+
     return (
     <Snackbar
-        open={true}
-        message="I love snacks"
+        open={snackbar.show}
+        message={snackbar.message}
     />
     )
 } 
