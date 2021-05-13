e7c64dfcab43a3d3e4e9decc836b731871dfcb12
This-is-ellesar <54068776+This-is-ellesar@users.noreply.github.com>
2021-05-13 05:31:44 +0300

    Поправил интерфейс аксиоса

--- src/http/axios-config.ts
+++ src/http/axios-config.ts
@@ -2,8 +2,8 @@ import Axios from 'axios'
 
 Axios.defaults.headers.common['Authorization'] = `Token ${localStorage.authorization_token}`;
 
-export const $axios = Axios.create({
-    baseURL: `http://localhost:3000/`,
+export const $axios:any = Axios.create({
+    baseURL: `http://localhost:5000/`,
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin' : '*'
