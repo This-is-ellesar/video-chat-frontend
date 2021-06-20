e91cce64dd7073b8988282c0214e9d76162bacbf
This-is-ellesar <armen16091998@icloud.com>
2021-06-20 19:41:17 +0300

    Обновил стили

--- src/views/Rooms/style.tsx
+++ src/views/Rooms/style.tsx
@@ -1,45 +1,47 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-export const Room = styled.div `
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 100px 1fr;
-    justify-items: center;
-    width: 100%;
-    height: calc(100vh - 70px);
-    h1{
-        color: #000;
-        font-family: 'Roboto';
-        font-weight: 400;
-    }
-`
-export const RoomList = styled.div `
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    padding: 30px;
-    width: 80%;
-    height: max-content;
-    min-height: 400px;
-    background: #fff;
-    box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
-    border-radius: 4px;
-`
+export const Room = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 100px 1fr;
+  justify-items: center;
+  width: 100%;
+  height: calc(100vh - 70px);
+  h1 {
+    color: #000;
+    font-family: 'Roboto';
+    font-weight: 400;
+  }
+`;
+export const RoomList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 30px;
+  width: 80%;
+  height: max-content;
+  min-height: 400px;
+  background: #fff;
+  box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
+  border-radius: 4px;
+`;
 
-export const RoomListItem = styled.div `
-    width: 250px;
-    height: 200px;
-    margin: 20px;
-    background: #fff;
-    border-radius: 4px;
-    box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
-    cursor: pointer;
-    transition: .1s ease-in;
+export const RoomListItem = styled.div`
+  width: 25%;
+  height: 200px;
+  margin: 20px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
+  cursor: pointer;
+  transition: 0.1s ease-in;
 
-    &:hover {
-        box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 40%), 0px 4px 5px 0px rgb(0 0 0 / 20%), 0px 1px 10px 0px rgb(0 0 0 / 15%);
-    }
-    &:active {
-        transform: scale(0.98);
-    }
-`
+  &:hover {
+    box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 40%),
+      0px 4px 5px 0px rgb(0 0 0 / 20%), 0px 1px 10px 0px rgb(0 0 0 / 15%);
+  }
+  &:active {
+    transform: scale(0.98);
+  }
+`;
