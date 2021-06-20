33c9819b81477a3861780ef263205daab20f2a87
This-is-ellesar <armen16091998@icloud.com>
2021-06-20 20:09:24 +0300

    Добавил модуль комнат для стора

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -4,15 +4,19 @@ import thunk from 'redux-thunk'
 //modules
 import { authReducer } from './auth'
 import { baseReducer } from './base'
+import { roomReducer } from './room'
 
 const rootReducer = combineReducers({
-    auth: authReducer,
-    base: baseReducer
+  auth: authReducer,
+  base: baseReducer,
+  room: roomReducer,
 })
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+)
 
 export type RootState = ReturnType<typeof rootReducer>
-
 
 export default store
