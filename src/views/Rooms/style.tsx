bad5762f4348c6ea60b07981ab9453122ccbfbcd
This-is-ellesar <armen16091998@icloud.com>
2021-06-20 20:09:41 +0300

    Поправил стили

--- src/views/Rooms/style.tsx
+++ src/views/Rooms/style.tsx
@@ -1,10 +1,10 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Room = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 100px 1fr;
   justify-items: center;
+  align-items: center;
   width: 100%;
   height: calc(100vh - 70px);
   h1 {
@@ -12,19 +12,19 @@ export const Room = styled.div`
     font-family: 'Roboto';
     font-weight: 400;
   }
-`;
+`
 export const RoomList = styled.div`
   display: flex;
   flex-wrap: wrap;
   padding: 30px;
-  width: 80%;
+  width: 100%;
   height: max-content;
   min-height: 400px;
   background: #fff;
   box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
     0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
   border-radius: 4px;
-`;
+`
 
 export const RoomListItem = styled.div`
   width: 25%;
@@ -44,4 +44,4 @@ export const RoomListItem = styled.div`
   &:active {
     transform: scale(0.98);
   }
-`;
+`
