6578baf56ce1e0a6bcc0b7c164494fbc2c371f9f
This-is-ellesar <armen16091998@icloud.com>
2021-06-20 20:09:34 +0300

    Поправил модуль стора для комнаты

--- src/store/room.tsx
+++ src/store/room.tsx
@@ -1,24 +1,35 @@
-import { IBaseState, IRoom } from '../types/store/room';
+import { IBaseState, IRoom } from '../types/store/room'
 
-const CREATE_ROOMS = 'CREATE_ROOMS';
+const CREATE_ROOMS = 'CREATE_ROOMS'
+const SET_ROOMS = 'SET_ROOMS'
 
 const defaultState: IBaseState = {
   rooms: [],
-};
+}
 
-export const baseReducer = (state: IBaseState = defaultState, action: any) => {
+export const roomReducer = (state: IBaseState = defaultState, action: any) => {
   switch (action.type) {
     case CREATE_ROOMS:
       return {
         ...state,
         snackbar: action.payload,
-      };
+      }
+    case SET_ROOMS:
+      return {
+        ...state,
+        rooms: action.payload,
+      }
     default:
-      return state;
+      return state
   }
-};
+}
+
+export const setRooms = (rooms: IRoom[]) => ({
+  type: SET_ROOMS,
+  payload: rooms,
+})
 
 export const createRooms = (room: IRoom) => ({
   type: CREATE_ROOMS,
   payload: room,
-});
+})
