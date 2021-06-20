d54b181693d69ebb65a1bb79487605d4260ed2dd
This-is-ellesar <armen16091998@icloud.com>
2021-06-20 11:20:05 +0300

    Поправил шапку

--- src/components/base/AppHeader.tsx
+++ src/components/base/AppHeader.tsx
@@ -1,43 +1,31 @@
-import { FC } from 'react'
-import { useDispatch } from 'react-redux'
-import { showSnackbarReducer } from '../../store/actions/snackbar'
-import { NavLink } from 'react-router-dom'
-//ui 
-import { HeaderComponents, HeaderContainerItem, HeaderContainer } from './style'
+import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+//ui
+import {
+  HeaderComponents,
+  HeaderContainerItem,
+  HeaderContainer,
+} from './style';
 //icons
-import Logo from '../../assets/icons/logo.svg'
+import Logo from '../../assets/icons/logo.svg';
 //interfaces
 import { IHeader } from './App-types';
 
 const Header: FC<IHeader> = () => {
+  return (
+    <HeaderComponents>
+      <HeaderContainer>
+        <HeaderContainerItem>
+          <NavLink to="/">
+            <img src={Logo} alt="Логотип" />
+          </NavLink>
+        </HeaderContainerItem>
+        <HeaderContainerItem>
+          <NavLink to="/sign">Sign In</NavLink>
+        </HeaderContainerItem>
+      </HeaderContainer>
+    </HeaderComponents>
+  );
+};
 
-    function SHOW_SNACKBAR(){
-        // @ts-ignore: Unreachable code error
-        useDispatch()
-    }
-    
-    return (
-        <HeaderComponents>
-            <HeaderContainer>
-                <HeaderContainerItem>
-                    <NavLink to="/">
-                        <img 
-                        src={Logo} 
-                        alt="Логотип"
-                        />
-                    </NavLink>
-                </HeaderContainerItem>
-                <HeaderContainerItem>
-                    <button onClick={() => SHOW_SNACKBAR()}>Click</button>
-                </HeaderContainerItem>
-                <HeaderContainerItem>
-                    <NavLink to="/sign">
-                        Sign In
-                    </NavLink>
-                </HeaderContainerItem>
-            </HeaderContainer>
-        </HeaderComponents>
-    )
-}
-
-export default Header
+export default Header;
