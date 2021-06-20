3f720ef5e8f5b976976f6fc5137dbe4907de4138
This-is-ellesar <armen16091998@icloud.com>
2021-06-20 16:41:00 +0300

    Поправил входной компонент

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,35 @@
 import { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Route } from 'react-router-dom';
 //views
-import Rooms from './views/Rooms/Rooms'
-import Auth from './views/Auth/SignIn'
+import Rooms from './views/Rooms/Rooms';
+import Auth from './views/Auth/SignIn';
 //components
-import Header from './components/base/AppHeader'
-import Dialog from './components/base/AppDialog'
-import SnackBar from './components/base/AppSnackBar'
-import NotFoundPage from './views/Errors/NotFound'
+import Header from './components/base/AppHeader';
+import Dialog from './components/base/AppDialog';
+import SnackBar from './components/base/AppSnackBar';
+import NotFoundPage from './views/Errors/NotFound';
 //ui
-import { MainWrapper } from './components/base/style'
+import { MainWrapper } from './components/base/style';
 //types
 import { IAppProps, IAppState } from './components/base/App-types';
 
 class App extends Component<IAppProps, IAppState> {
-
-  render(){
+  render() {
     return (
       <>
         <Header />
-  
+
         <MainWrapper>
           <Route path="/" exact component={Rooms} />
           <Route path="/sign" component={Auth} />
-          <Route component={NotFoundPage} />
+          <Route path="*" component={NotFoundPage} />
         </MainWrapper>
-       
+
         <Dialog open={false} />
         <SnackBar />
       </>
-    )
-
+    );
   }
-
 }
 
 export default App;
