45bbe39e06d8ca32256146235f9728dd68030e5e
This-is-ellesar <armen16091998@icloud.com>
2021-06-20 11:20:34 +0300

    Поправил модуль аутентификации в сторе

--- src/store/auth.tsx
+++ src/store/auth.tsx
@@ -1,45 +1,45 @@
-import { IAuthState } from './store-types'
+import { IAuthState } from '../types/store/auth';
 
-const GET_TOKEN = "GET_TOKEN"
-const SET_USER = 'SET_USER'
+const GET_TOKEN = 'GET_TOKEN';
+const SET_USER = 'SET_USER';
 
-const defaultState:IAuthState = {
-    user: {
-        _id: '',
-        name: '',
-        surname: '',
-        nickname: '',
-        roles: []
-    },
-    token: '',
-    isAuthtentificated: false,
-    isFetching: false,
-}
+const defaultState: IAuthState = {
+  user: {
+    _id: '',
+    name: '',
+    surname: '',
+    nickname: '',
+    roles: [],
+  },
+  token: '',
+  isAuthtentificated: false,
+  isFetching: false,
+};
 
 export const authReducer = (state: IAuthState = defaultState, action: any) => {
-    switch(action.type){ 
-        case SET_USER: 
-            return {
-                ...state,
-                user: action.payload.user,
-                token: action.payload.token,
-                isAuthtentificated: true
-            }
-        case GET_TOKEN:
-            return {
-                ...state,
-                token: action.payload
-            }
-        default :
-            return state
-    }
-}
+  switch (action.type) {
+    case SET_USER:
+      return {
+        ...state,
+        user: action.payload.user,
+        token: action.payload.token,
+        isAuthtentificated: true,
+      };
+    case GET_TOKEN:
+      return {
+        ...state,
+        token: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 export const setUser = (data: IAuthState) => ({
-    type: SET_USER,
-    payload: data
-})
+  type: SET_USER,
+  payload: data,
+});
 
-export const getUsers = (token : string) => ({
-    type: GET_TOKEN, 
-    payload: token
-})
+export const getUsers = (token: string) => ({
+  type: GET_TOKEN,
+  payload: token,
+});
