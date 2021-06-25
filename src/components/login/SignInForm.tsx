4811f28f7696801318caafad486990317436235c
This-is-ellesar <armen16091998@icloud.com>
2021-06-25 23:28:02 +0300

    Формат кода

--- src/components/login/SignInForm.tsx
+++ src/components/login/SignInForm.tsx
@@ -1,64 +1,67 @@
 import { FC, useState } from 'react'
 import { $axios } from '../../http/axios-config'
 //store
-//ui 
+//ui
 import { Button, TextField } from '@material-ui/core'
-import { SignIn, InputWrapper, InputWrapperHeadline, InputWrapperItem } from './style'
-
+import {
+  SignIn,
+  InputWrapper,
+  InputWrapperHeadline,
+  InputWrapperItem,
+} from './style'
 
 const DialogSignIn: FC = () => {
-    const [ name, setName ] = useState('')
-    const [ password, setPassword ] = useState('')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
 
-    const signIn = async (name: string, password: string) => {
-        try {
-            const { data } = await $axios.post('/auth/login/', {
-                name,
-                password
-            })
-
-            console.log(data)
-
-        } catch (error) {
-            console.log(error)
-        }
+  const signIn = async (name: string, password: string) => {
+    try {
+      const { data } = await $axios.post('/auth/login/', {
+        name,
+        password,
+      })
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    return (
-        <SignIn>
-            <InputWrapper>
+  return (
+    <SignIn>
+      <InputWrapper>
+        <InputWrapperHeadline>Login Form</InputWrapperHeadline>
 
-                <InputWrapperHeadline>Login Form</InputWrapperHeadline>
+        <form noValidate autoComplete="off">
+          <InputWrapperItem>
+            <TextField
+              label="Name"
+              variant="outlined"
+              margin="dense"
+              onChange={(event) => setName(event.target.value)}
+            />
+          </InputWrapperItem>
 
-                <form noValidate autoComplete="off">
-                    <InputWrapperItem>
-                        <TextField 
-                        label="Name" 
-                        variant="outlined" 
-                        margin="dense"
-                        onChange={(event) => setName(event.target.value)}
-                        />
-                    </InputWrapperItem>
-
-                    <InputWrapperItem>
-                        <TextField 
-                        type="password"
-                        label="Password" 
-                        variant="outlined" 
-                        margin="dense"
-                        onChange={(event) => setPassword(event.target.value)}
-                        />
-                    </InputWrapperItem>
-                    <InputWrapperItem>
-                    
-                        <Button onClick={() => signIn(name, password)} color="primary" variant="contained">Continue</Button>
-                    </InputWrapperItem>
-                </form>
-                
-            </InputWrapper>
-        </SignIn>
-    )
+          <InputWrapperItem>
+            <TextField
+              type="password"
+              label="Password"
+              variant="outlined"
+              margin="dense"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </InputWrapperItem>
+          <InputWrapperItem>
+            <Button
+              onClick={() => signIn(name, password)}
+              color="primary"
+              variant="contained"
+            >
+              Continue
+            </Button>
+          </InputWrapperItem>
+        </form>
+      </InputWrapper>
+    </SignIn>
+  )
 }
-
 
 export default DialogSignIn
