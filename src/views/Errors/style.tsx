0a59e078486b7f5ebc9c588f3cbc1ecbac806406
This-is-ellesar <armen16091998@icloud.com>
2021-06-26 15:51:20 +0300

    Поправил стили

--- src/views/Errors/style.tsx
+++ src/views/Errors/style.tsx
@@ -1,12 +1,10 @@
 import styled from 'styled-components'
 
-export const NotFoundContainer = styled.div `
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: calc(100vh - 70px);
-    h1{ 
-        margin: 0;
-    }
+export const NotFoundContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: calc(100vh - 70px);
+  text-align: center;
 `
