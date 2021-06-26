f80bd978a8e004b42412ff50b300b4b3096c8e4e
This-is-ellesar <armen16091998@icloud.com>
2021-06-26 15:52:04 +0300

    Стили компонента комнаты

--- src/components/room/style.tsx
+++ src/components/room/style.tsx
@@ -3,20 +3,35 @@ import styled from 'styled-components'
 export const RoomItemHeader = styled.header`
   display: flex;
   justify-content: space-between;
-  padding: 10px;
+  align-items: center;
+  padding: 15px 10px;
   h1 {
     margin: 0;
+    font-family: 'Roboto';
     font-size: 18px;
-    font-weight: 600;
+    font-weight: 500;
   }
   img {
-    cursor: pointer;
+    display: none;
+  }
+  &:hover {
+    img {
+      display: block;
+      cursor: pointer;
+    }
   }
 `
 export const RoomItemMain = styled.main`
   padding: 10px;
+  overflow: auto;
+  font-family: 'Roboto';
+  font-size: 14px;
 `
 export const RoomItemFooter = styled.footer`
-  padding: 10px;
-  font-size: 14px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 10px 10px;
+  font-size: 16px;
+  font-family: 'Roboto';
 `
