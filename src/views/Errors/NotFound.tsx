9b80635303c8d022dfc3a0e5f2f2f0f0b9d7839f
This-is-ellesar <armen16091998@icloud.com>
2021-06-26 15:52:17 +0300

    Поправил компонент 404 ошибки

--- src/views/Errors/NotFound.tsx
+++ src/views/Errors/NotFound.tsx
@@ -1,14 +1,17 @@
 import { FC } from 'react'
+import { NavLink } from 'react-router-dom'
 //ui
 import { NotFoundContainer } from './style'
 
-
 const NotFoundPage: FC = () => {
-    return (
-        <NotFoundContainer>
-            <h1>404 Not Found</h1>
-        </NotFoundContainer>
-    )
+  return (
+    <NotFoundContainer>
+      <div>
+        <h1>404 Not Found</h1>
+        <NavLink to="/">На главную</NavLink>
+      </div>
+    </NotFoundContainer>
+  )
 }
 
 export default NotFoundPage
