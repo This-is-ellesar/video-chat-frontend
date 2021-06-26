a89aebeb9a0a723df84905ced517fb3f154a286a
This-is-ellesar <armen16091998@icloud.com>
2021-06-26 23:05:22 +0300

    Поправил стили

--- src/views/Rooms/style.tsx
+++ src/views/Rooms/style.tsx
@@ -1,26 +1,28 @@
 import styled from 'styled-components'
 
 export const Room = styled.div`
-  display: grid;
-  grid-template-rows: max-content 1fr;
-  grid-template-columns: 1fr;
-  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   height: calc(100vh - 70px);
   h1 {
-    padding: 20px 0;
-    color: #000;
     font-family: 'Roboto';
     font-weight: 400;
   }
 `
+export const RoomHeaderStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+`
+
 export const RoomContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  padding: 30px;
-  width: 100%;
-  height: max-content;
+  width: 80%;
   min-height: 400px;
-  background: #fff;
+  background: #ccc;
   box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
     0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
   border-radius: 4px;
@@ -34,12 +36,12 @@ export const RoomListItem = styled.div`
   margin: 20px;
   background: #fff;
   border-radius: 4px;
-  box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
-    0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
+  box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 10%),
+    0px 4px 5px 0px rgb(0 0 0 / 10%), 0px 1px 10px 0px rgb(0 0 0 / 10%);
   transition: 0.1s ease-in;
 
   &:hover {
-    box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 40%),
+    box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
       0px 4px 5px 0px rgb(0 0 0 / 20%), 0px 1px 10px 0px rgb(0 0 0 / 15%);
   }
 `
