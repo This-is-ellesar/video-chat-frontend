6677097fc65ff6f0d23485769e46e3c6d5630973
This-is-ellesar <armen16091998@icloud.com>
2021-06-26 16:10:51 +0300

    Поправил компонент списка комнат

--- src/components/room/RoomList.tsx
+++ src/components/room/RoomList.tsx
@@ -1,17 +1,12 @@
 import { FC } from 'react'
-//hooks
-import { useTypedSelector } from '../hooks/useTypedSelector'
 //components
 import RoomListItem from './RoomListItem'
 //ui
 import { RoomContainer } from '../../views/Rooms/style'
 
 const RoomList: FC = () => {
-  const { users } = useTypedSelector((state) => state.room.rooms)
-
   return (
     <RoomContainer>
-      {users}
       <RoomListItem />
     </RoomContainer>
   )
