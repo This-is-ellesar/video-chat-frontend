eba8660af62598621bda3b0b31194bec082d07a9
This-is-ellesar <armen16091998@icloud.com>
2021-06-26 16:10:30 +0300

    Форматирование хедера

--- src/components/base/AppHeader.tsx
+++ src/components/base/AppHeader.tsx
@@ -1,15 +1,12 @@
-import { FC } from 'react';
-import { NavLink } from 'react-router-dom';
+import { FC } from 'react'
+import { NavLink } from 'react-router-dom'
 //ui
-import {
-  HeaderComponents,
-  HeaderContainerItem,
-  HeaderContainer,
-} from './style';
+import { HeaderComponents, HeaderContainerItem, HeaderContainer } from './style'
 //icons
-import Logo from '../../assets/icons/logo.svg';
+import Logo from '../../assets/icons/logo.svg'
+import AccountIcon from '../../assets/icons/account-icon.svg'
 //interfaces
-import { IHeader } from './App-types';
+import { IHeader } from './App-types'
 
 const Header: FC<IHeader> = () => {
   return (
@@ -21,11 +18,13 @@ const Header: FC<IHeader> = () => {
           </NavLink>
         </HeaderContainerItem>
         <HeaderContainerItem>
-          <NavLink to="/sign">Sign In</NavLink>
+          <NavLink to="/sign">
+            <img src={AccountIcon} alt="Sign in" title="Sign in" />
+          </NavLink>
         </HeaderContainerItem>
       </HeaderContainer>
     </HeaderComponents>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
