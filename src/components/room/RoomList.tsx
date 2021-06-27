a355a7f0348f4700168284121136f39130535d40
This-is-ellesar <armen16091998@icloud.com>
2021-06-27 23:36:25 +0300

    Допилил спискок комнат

--- src/components/room/RoomList.tsx
+++ src/components/room/RoomList.tsx
@@ -1,17 +1,21 @@
 import { FC } from 'react'
+import { useTypedSelector } from '../hooks/useTypedSelector'
 //components
 import RoomListItem from './RoomListItem'
 //ui
 import { RoomContainer } from '../../views/Rooms/style'
+//types
+import { IRoom } from '../../types/store/room'
 
 const RoomList: FC = () => {
+  const { rooms } = useTypedSelector((state) => state.room)
+
   return (
     <RoomContainer>
-      <RoomListItem />
-      <RoomListItem />
-      <RoomListItem />
-      <RoomListItem />
-      <RoomListItem />
+      {rooms.map((room: IRoom) => (
+        // @ts-ignore: Unreachable code error
+        <RoomListItem room={room} key={room.id} />
+      ))}
     </RoomContainer>
   )
 }
