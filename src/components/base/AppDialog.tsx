45621c31680e95c29da147affdadd23ff88683be
This-is-ellesar <armen16091998@icloud.com>
2021-06-29 19:03:05 +0300

    Форматирование кода

--- src/components/base/AppDialog.tsx
+++ src/components/base/AppDialog.tsx
@@ -1,25 +1,21 @@
 import { FC } from 'react'
 
-import { Dialog, DialogContent, DialogTitle } from '@material-ui/core/';
+import { Dialog, DialogContent, DialogTitle } from '@material-ui/core/'
 import { DialogContainer, Content } from './style'
 
 import { IDialog } from './App-types'
 
 const AppDialog: FC<IDialog> = (props) => {
-    return (
-        <Dialog
-        open={props.open}
-        >
-            <DialogTitle id="alert-dialog-title">Вход</DialogTitle>
-            <DialogContent>
-                <DialogContainer>
-                    <Content>
-                        {props.dialogContent}
-                    </Content>
-                </DialogContainer>
-            </DialogContent>
-        </Dialog>
-    )
+  return (
+    <Dialog open={props.open}>
+      <DialogTitle id="alert-dialog-title">Вход</DialogTitle>
+      <DialogContent>
+        <DialogContainer>
+          <Content>{props.dialogContent}</Content>
+        </DialogContainer>
+      </DialogContent>
+    </Dialog>
+  )
 }
 
-export default AppDialog 
+export default AppDialog
