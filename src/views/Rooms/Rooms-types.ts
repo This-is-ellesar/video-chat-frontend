38fa211e6b7fb29f4774d53c9afc72cf05204609
This-is-ellesar <armen16091998@icloud.com>
2021-06-29 19:09:17 +0300

    Обновил типы

--- src/views/Rooms/Rooms-types.ts
+++ src/views/Rooms/Rooms-types.ts
@@ -1,10 +1,5 @@
-export interface IProps {
-  showSnackbar: (arg0: any) => void
-  setRooms: (arg0: any) => void
-}
-export interface IState {
-  rooms: IRoom[]
-}
+export interface IProps {}
+export interface IState {}
 
 export interface IRoom {
   id: number | null
