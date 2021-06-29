8673097c05d53cc3d01ba698400d6c549c69a5f4
This-is-ellesar <armen16091998@icloud.com>
2021-06-29 18:34:26 +0300

    Добавил страницу выбранной комнаты

---
+++ src/views/CurrentRoom/CurrentRoom.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+//ui
+import { CurrentRoomContainer } from './style'
+//types
+import { IProps, IState } from './current-room-types'
+
+class CurrentRoom extends Component<IProps, IState> {
+  render() {
+    return <CurrentRoomContainer>dadaw</CurrentRoomContainer>
+  }
+}
+
+export default CurrentRoom
