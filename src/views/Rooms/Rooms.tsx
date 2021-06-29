6776c930ef035cbcb7899e3594c1afbe6d3d736c
This-is-ellesar <armen16091998@icloud.com>
2021-06-29 19:09:25 +0300

    Удалил работу со стором

--- src/views/Rooms/Rooms.tsx
+++ src/views/Rooms/Rooms.tsx
@@ -1,33 +1,13 @@
 import { Component } from 'react'
-import { connect } from 'react-redux'
 //components
 import RoomHeader from '../../components/room/RoomHeader'
 import RoomList from '../../components/room/RoomList'
-//store
-import { setRooms } from '../../store/room'
-import { showSnackbar } from '../../store/base'
 //ui
 import { Room } from './style'
 //types
 import { IProps, IState } from './Rooms-types'
-//http
-import { $axios } from '../../http/axios-config'
 
 class Rooms extends Component<IProps, IState> {
-  async componentWillMount() {
-    try {
-      const { data } = await $axios.get('/room/rooms/')
-
-      this.props.setRooms(data)
-    } catch (e) {
-      this.props.showSnackbar({
-        message: 'Упс! Произошла ошибка, пожалуйста, попробуйте позже!',
-        show: true,
-        type: 'message',
-      })
-    }
-  }
-
   render() {
     return (
       <Room>
@@ -38,9 +18,4 @@ class Rooms extends Component<IProps, IState> {
   }
 }
 
-const mapDispatchProps = {
-  setRooms,
-  showSnackbar,
-}
-
-export default connect(null, mapDispatchProps)(Rooms)
+export default Rooms
